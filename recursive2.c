554d8cdcec0c6d2be69a4e5f30e0a54cfbd88eac
Şeyda Arı <ariseydaari@gmail.com>
2020-06-27 14:57:49 +0300

    Recursive addition function
     Write C code for a program that calculates the sum of natural numbers until the number that is entered by user. Calculation must be by using recursive function.

---
+++ recursive2.c
@@ -0,0 +1,26 @@
+#include<stdio.h>
+
+//Write C code for a program that calculates the sum of natural numbers until the number that is entered by user. 
+//Calculation must be by using recursive function.  
+
+int item_sum(int n){
+	
+	if(n<1){
+		return 0;
+	}
+	
+	return(n+item_sum(n-1));
+	
+}
+int main(){
+	
+	int a;
+	printf("Please enter an integer.\nEntered number:");
+	scanf("%d",&a);
+	
+	int result=item_sum(a);
+	printf("Sum of numbers until the entered number:%d",result);
+	
+	return 0;
+}
+
